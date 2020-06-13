cdd336aaf891e1446026bdc4e5a37ce8a095c010
onwl007 <onwl007@126.com>
2020-06-13 17:57:01 +0800

    map 类型

--- loop.go
+++ loop.go
@@ -176,4 +176,96 @@ LOOP:
 		sum += v
 	}
 	fmt.Println("\nsum of all elements of a", sum)
+
+	for _, v := range arr7 {
+		fmt.Printf("the element of a is %.2f\n", v)
+	}
+
+	arr9 := [3][4]int{
+		{0, 1, 2, 3},
+		{4, 5, 6, 7},
+		{8, 9, 10, 11},
+	}
+	fmt.Println(arr9)
+
+	arr10 := [...]string{"USA", "China", "India", "Germany", "France"}
+	arr11 := arr10
+	arr11[0] = "Singapore"
+	fmt.Println("arr10 is ", arr10)
+	fmt.Println("arr11 is ", arr11)
+
+	// 数组的大小也是类型的一部分，因此数组不能被调整大小，不要担心这个限制，因为切片的存在就是为了解决这个问题
+	//arr12 := [3]int{5, 78, 8}
+	//var arr13 [5]int
+	//arr13 = arr12 // cannot use arr12 (type [3]int) as type [5]int in assignment
+
+	/**
+	切片
+		切片是一种方便、灵活且强大的包装器，本身没有任何数据，只是对现有数组的引用
+		切片更像是一个结构体，包含了三个元素
+			1.指针，指向数组中slice指定的开始位置
+			2.长度，即slice的长度
+			3.最大长度，也就是slice开始位置到数组的最后位置的长度
+	*/
+	arr12 := [5]int{76, 77, 78, 79, 80}
+	var arr13 []int = arr12[1:4]
+	fmt.Println(arr13)
+
+	// slice 没有自己的任何数据，只是底层数组的一个展示，对 slice 所做的任何修改都将反映在底层数组中
+	arr14 := [...]int{57, 89, 90, 82, 100, 78, 67, 69, 59}
+	arr14Slice := arr14[2:5]
+	fmt.Println("array before", arr14)
+	for i := range arr14Slice {
+		arr14Slice[i]++
+	}
+	fmt.Println("array after", arr14)
+
+	numa := [3]int{78, 79, 80}
+	nums1 := numa[:]
+	nums2 := numa[:]
+	fmt.Println("array before change 1", numa)
+	nums1[0] = 100
+	fmt.Println("array after modification to slice nums1", numa)
+	nums2[1] = 101
+	fmt.Println("array after modification to slice nums2", numa)
+
+	var slice1 = make([]int, 3, 5)
+	printSlice(slice1)
+
+	var slice2 []int
+	printSlice(slice2)
+	if slice2 == nil {
+		fmt.Println("切片是空的")
+	}
+
+	slice3 := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	printSlice(slice3)
+
+	//打印原始切片
+	fmt.Println("slice3 == ", slice3)
+
+	//打印子切片从索引1到索引4（不包含）
+	fmt.Println("slice3[1:4] == ", slice3[1:4])
+
+	//默认下限为0
+	fmt.Println("slice3[:3] == ", slice3[:3])
+
+	//默认上限为 len(s)
+	fmt.Println("slice3[4:] == ", slice3[4:])
+
+	slice4 := make([]int, 0, 5)
+	printSlice(slice4)
+
+	//打印子切片从索引0到索引2
+	slice5 := slice3[:2]
+	printSlice(slice5)
+
+	//打印子切片从索引2到索引5
+	slice6 := slice3[2:5]
+	printSlice(slice6)
+
+}
+
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
 }
